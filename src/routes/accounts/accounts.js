a06d29c7562999a53cec8fd444a7b9c897f10fd9
Damian Nolan <damiannolan@gmail.com>
2018-04-07 20:37:33 +0100

    updated config, added createPerson and addFace functions

--- src/routes/accounts/accounts.js
+++ src/routes/accounts/accounts.js
@@ -8,10 +8,69 @@ export const bootstrap = (server) => {
     // server.post('/accounts/login');
 };
 
-const endpoint = config.get('Services.Microsoft.FaceAPI.endpoint');
+const baseURL = config.get('Services.Microsoft.FaceAPI.baseURL');
 const apiKey = config.get('Services.Microsoft.FaceAPI.apiKey1');
+const personGroupId = config.get('Services.Microsoft.FaceAPI.personGroupId');
 
-const register = async (req, res, next) => {
+const register = async (req, res) => {
+    try {
+        const personId = await createPerson(req.body.username);
+        const persistedFaceId = await addFace(personId, req.body.imageUrl);
+
+        res.send(200, { personId: personId, persistedFaceId: persistedFaceId });
+    } catch (e) {
+        log.error(`Failed to find face for ${req.body.username}`);
+    }
+};
+
+const createPerson = async (username) => {
+    const options = {
+        body: {
+            name: username
+        },
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': `${apiKey}`
+        },
+        json: true,
+        method: 'POST',
+        resolveWithFullResponse: true,
+        url: `${baseURL}/persongroups/${personGroupId}/persons`
+    };
+
+    try {
+        const response = await request(options);
+        log.debug('Person Created -> Logging resopnse from MS', response.body);
+
+        return response.body.personId;
+    } catch (e) {
+        log.error(e);
+    }
+};
+
+const addFace = async (personId, imageUrl) => {
+    const options = {
+        body: Buffer.from(imageUrl.split(',')[1], 'base64'),
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': `${apiKey}`
+        },
+        method: 'POST',
+        resolveWithFullResponse: true,
+        url: `${baseURL}/persongroups/${personGroupId}/persons/${personId}/persistedFaces`
+    };
+
+    try {
+        const response = await request(options);
+        log.debug('Added Face -> Logging response from MS', response.body);
+
+        return response.body.persistedFaceId;
+    } catch (e) {
+        log.error(e);
+    }
+};
+
+const detectFace = async (req, res, next) => {
     const username = req.body.username;
     const imageUrl = req.body.imageUrl;
     
@@ -22,9 +81,8 @@ const register = async (req, res, next) => {
             'Ocp-Apim-Subscription-Key': `${apiKey}`,
         },
         method: 'POST',
-        processData: false,
         resolveWithFullResponse: true,
-        url: `${endpoint}/detect`,
+        url: `${baseURL}/detect`,
     };
 
     try {
