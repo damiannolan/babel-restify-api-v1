88f4b52282decd8d1c161f8b13d4810f93bde5bf
Damian Nolan <damiannolan@gmail.com>
2018-04-07 22:06:34 +0100

    setup login functionality and fixed the JSON parsing issue with response from application/octet-stream requests

--- src/routes/accounts/accounts.js
+++ src/routes/accounts/accounts.js
@@ -5,7 +5,7 @@ import { logger as log } from '../../logger';
 
 export const bootstrap = (server) => {
     server.post('/accounts/register', register);
-    // server.post('/accounts/login');
+    server.post('/accounts/login', login);
 };
 
 const baseURL = config.get('Services.Microsoft.FaceAPI.baseURL');
@@ -17,9 +17,28 @@ const register = async (req, res) => {
         const personId = await createPerson(req.body.username);
         const persistedFaceId = await addFace(personId, req.body.imageUrl);
 
-        res.send(200, { personId: personId, persistedFaceId: persistedFaceId });
+        const response = { // temperorary
+            personId: personId,
+            persistedFaceId: persistedFaceId
+        };
+
+        res.send(201, response);
     } catch (e) {
         log.error(`Failed to find face for ${req.body.username}`);
+    }
+};
+
+const login = async (req, res) => {
+    try {
+        const faceId = await detectFace(req.body.imageUrl);
+        log.debug('Log Debugging Face Id', faceId);
+        const personId = 'b6177b61-e3e1-4fe6-963e-476f7613f945'; // collect from db
+
+        const verify = await verifyFace(faceId, personId);
+
+        res.send(200, { result: 'success' });
+    } catch (e) {
+        log.error(e);
     }
 };
 
@@ -62,18 +81,16 @@ const addFace = async (personId, imageUrl) => {
 
     try {
         const response = await request(options);
-        log.debug('Added Face -> Logging response from MS', response.body);
+        const msResp = JSON.parse(response.body);
+        log.debug('Added Face -> Logging response from MS', msResp);
 
-        return response.body.persistedFaceId;
+        return msResp.persistedFaceId;
     } catch (e) {
         log.error(e);
     }
 };
 
-const detectFace = async (req, res, next) => {
-    const username = req.body.username;
-    const imageUrl = req.body.imageUrl;
-    
+const detectFace = async (imageUrl) => {
     const options = {
         body: Buffer.from(imageUrl.split(',')[1], 'base64'),
         headers: {
@@ -87,8 +104,40 @@ const detectFace = async (req, res, next) => {
 
     try {
         const response = await request(options);
-        log.debug('Logging response from microsoft', response.body);
+        const msResp = JSON.parse(response.body);
+        log.debug('Logging response from microsoft', msResp);
+
+        return msResp[0].faceId;
     } catch (e) {
         log.error(e);
+    }
+};
+
+const verifyFace = async (faceId, personId) => {
+    const payload = {
+        faceId: faceId,
+        personId: personId,
+        personGroupId: personGroupId
+    };
+
+    const options = {
+        body: payload,
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': `${apiKey}`
+        },
+        json: true,
+        method: 'POST',
+        resolveWithFullResponse: true,
+        url: `${baseURL}/verify`
+    };
+
+    try {
+        const response = await request(options);
+        log.debug('Face Verify -> Logging resopnse from MS', response.body);
+
+        return response.body;
+    } catch (e) {
+        log.error('Problem verifying face', e);
     }
 };
