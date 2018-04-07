0800af8997364ea0d6aa4407143c8f55afa51e09
Damian Nolan <damiannolan@gmail.com>
2018-04-07 19:43:46 +0100

    sanity commit, detection now working using base64 image url in request. tested via postman/curl

--- src/routes/accounts/accounts.js
+++ src/routes/accounts/accounts.js
@@ -1,8 +1,36 @@
 import * as restify from 'restify';
-
+import request from 'request-promise-native';
 import config from 'config';
+import { logger as log } from '../../logger';
 
 export const bootstrap = (server) => {
-    server.post('/accounts/register');
-    server.post('/accounts/login');
+    server.post('/accounts/register', register);
+    // server.post('/accounts/login');
+};
+
+const endpoint = config.get('Services.Microsoft.FaceAPI.endpoint');
+const apiKey = config.get('Services.Microsoft.FaceAPI.apiKey1');
+
+const register = async (req, res, next) => {
+    const username = req.body.username;
+    const imageUrl = req.body.imageUrl;
+    
+    const options = {
+        body: Buffer.from(imageUrl.split(',')[1], 'base64'),
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': `${apiKey}`,
+        },
+        method: 'POST',
+        processData: false,
+        resolveWithFullResponse: true,
+        url: `${endpoint}/detect`,
+    };
+
+    try {
+        const response = await request(options);
+        log.debug('Logging response from microsoft', response.body);
+    } catch (e) {
+        log.error(e);
+    }
 };
